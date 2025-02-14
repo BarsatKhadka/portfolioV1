9832028c525a41761ffd884a80f7b43d8ab16a56
barsatKhadka <khadkabarsat598@gmail.com>
2025-02-14 14:02:01 -0600

    resume update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
       </span>
     </p>
     
-    <a href="https://drive.google.com/uc?export=download&id=1ubUzCBNGE_ovCoD2ExKqsK6laWR4JCfA" className="text-[#ebffff] underline mb-4 lg:mb-0 lg:mr-64">
+    <a href="https://drive.google.com/uc?export=download&id=1wOP0CS3UARHHvoPbmoC4Q2dHNSnyMDn8" className="text-[#ebffff] underline mb-4 lg:mb-0 lg:mr-64">
       Resume
     </a>
   </div>
