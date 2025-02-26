ddac9447b379a5ecd76074467b77534b26153110
barsatKhadka <khadkabarsat598@gmail.com>
2025-02-25 20:53:57 -0600

    update: footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
-
 
 export const Footer = () => {
   return (
-    <footer className="bg-[#1C1917] border-t border-[#000000]/10 py-4 fixed  bottom-0 left-0 w-full sm:mt-4">
+    <footer className="bg-[#1C1917] border-t border-[#000000]/20 py-4 fixed bottom-0 left-0 w-full sm:mt-4 shadow-lg shadow-black/10">
       <div className="container mx-auto px-4 lg:px-48">
         <div className="flex items-center justify-between">
-          <p className="text-[#] text-sm">
-            &copy; {new Date().getFullYear()} Barsat Khadka.
+          <p className="text-gray-400 text-sm">
+            &copy; {new Date().getFullYear()} Barsat Khadka
+            <span className="hidden sm:inline ml-2">- Friend of Computer</span>
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex gap-5">
             <a
               href="https://github.com/barsatKhadka"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[#] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-gray-400 hover:text-[#eebc86] transition-all duration-300"
             >
               <FaGithub className="w-5 h-5" />
             </a>
@@ -24,7 +24,7 @@ export const Footer = () => {
               href="https://www.linkedin.com/in/barsat-khadka/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[#] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-gray-400 hover:text-[#eebc86] transition-all duration-300"
             >
               <FaLinkedin className="w-5 h-5"/>
             </a>
@@ -32,7 +32,7 @@ export const Footer = () => {
               href="https://leetcode.com/u/BarsatKhadka/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-gray-400 hover:text-[#eebc86] transition-all duration-300"
             >
               <SiLeetcode className="w-5 h-5" />
             </a>
