5ca304a8767dc0ce77f518ea3b94ba0dc7152b3c
barsatKhadka <khadkabarsat598@gmail.com>
2025-02-25 20:51:57 -0600

    add: lines beside experience and projects

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,8 +3,9 @@ import { MdOutlineWorkOutline } from "react-icons/md";
 export const Experience = () => {
   return (
     <div className="ml-3 mt-16 lg:ml-48">
-      <p className="text-[#ebffff] mb-6 ml-1">
+      <p className="text-[#ebffff] mb-6 ml-1 flex items-center">
         <MdOutlineWorkOutline className="inline mr-1"/> Experience
+        <span className="h-[1px] bg-gradient-to-r from-[#eebc86]/50 to-transparent ml-4 flex-grow"></span>
       </p>
       
       <div className="ml-2 lg:ml-6">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,9 @@ export const Projects = () => {
   return (
     <>
       <div className="ml-8 mt-16 lg:ml-48 ">
-        <p className="text-[#ebffff] mb-6"> 
+        <p className="text-[#ebffff] mb-6 flex items-center"> 
           <IoFolderOpenOutline className="inline mr-1" /> Projects 
+          <span className="h-[1px] bg-gradient-to-r from-[#eebc86]/50 to-transparent ml-4 flex-grow"></span>
         </p>
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mr-8 lg:mr-48">
