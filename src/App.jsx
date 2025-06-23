88bf788becab5f199204f2ac3b5a9687efdab797
BarsatKhadka <khadkabarsat598@gmail.com>
2025-06-23 09:53:54 -0500

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   // Initialize state from localStorage or default to false
@@ -52,6 +53,7 @@ function App() {
         </main>
         <Footer isDarkMode={isDarkMode} />
       </div>
+      <Analytics />
     </div>
   );
 }
